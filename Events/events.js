1f2aefc46076992c26f911c987d887bdee27cd72
Yash Reddy <yashmunurreddy63@gmail.com>
2023-12-24 13:36:19 +0530

    modified file events.html and added file events.js

---
+++ Events/events.js
@@ -0,0 +1,16 @@
+
+function upperCase(){
+    const y = document.getElementById("fname1");
+    y.value = y.value.toUpperCase();
+
+    const x = document.getElementById("fname");
+    x.value = x.value.toUpperCase();
+}
+
+function mOver(obj){
+    obj.style.background = "Yellow"
+}
+
+function mOut(obj){
+    obj.style.background = "Red";
+}
