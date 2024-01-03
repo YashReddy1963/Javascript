66e33b2a78b6000248c72abaafc093fdb1f403f9
Yash Reddy <yashmunurreddy63@gmail.com>
2024-01-03 22:47:15 +0530

    added folder promise and few changes in Async(html/js)

--- Async/async.js
+++ Async/async.js
@@ -36,40 +36,11 @@ function time(){//Asynchronous function
     }
 }
 
-function promise_fn(){
-    /*There are few promise objects:
-    Pending--undefined
-    Fullfilled--a result value
-    Rejected--an error object */
-
-    function myDisplayer(some){
-        document.getElementById("demo").innerHTML = some;
-    }
-
-    let myPromise = new Promise(
-        function(myResolve, myReject) {
-          let x = 0;
-        
-          if (x == 0) {
-            myResolve("OK");
-          } else {
-            myReject("Error");
-          }
-        }
-        );
-
-    myPromise.then(
-        function(value)
-        {myDisplayer(value);},
-        function(error)
-        {myDisplayer(error); }
-    );
-}
 
 function async_await(){
     //Keyword "async" before a funciton makes the function return a promise
     async function myDisplay(){
-        let myPromise = new Promise(function(resolve, reject){
+        let myPromise = new Promise((resolve, reject)=>{
             resolve("Heyo Yash is here!");
         });
         //Await keyword can only be used inside an async function
@@ -77,4 +48,8 @@ function async_await(){
         document.getElementById("demo").innerHTML = await myPromise;
     }
     myDisplay();
+}
+
+function async_await2(){
+
 }
---
+++ Promises/promise.js
@@ -0,0 +1,44 @@
+//Promise chaining
+let p1 = new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        console.log("This had printed after 2 second")
+        resolve(56)
+    },2000)
+})
+
+p1.then((value)=>{
+    console.log(value)
+    return new Promise((resolve, reject)=>{
+        resolve(53)
+    })
+}).then((value)=>{
+    console.log(value);
+    console.log("The value 53 is of second promise created using promise chaining")
+})
+
+//problem solving
+function promise_fn(){
+    /*There are few promise objects:
+    Pending--undefined
+    Fullfilled--a result value
+    Rejected--an error object */
+
+    function myDisplayer(some){
+        document.getElementById("demo").innerHTML = some;
+    }
+
+    let myPromise = new Promise((myResolve, myReject) =>{
+          let x = 0;
+        
+          if (x == 0) {
+            myResolve("OK");
+          } else {
+            myReject("Error");
+          }
+        }
+        );
+
+    myPromise.then((value)=> {myDisplayer(value);},
+            (error) => {myDisplayer(error); }
+    );
+}
